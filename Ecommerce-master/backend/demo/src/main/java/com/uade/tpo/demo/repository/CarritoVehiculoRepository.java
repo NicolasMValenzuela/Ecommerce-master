1b3051539c4d7698a88e67266a7aa26c93f82ec0
Juani Aguirre <juani.aguirremonti@gmail.com>
2025-10-18 18:19:22 -0300

    Elimino carpeta que no sirve

--- Ecommerce-master/backend/demo/src/main/java/com/uade/tpo/demo/repository/CarritoVehiculoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.uade.tpo.demo.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import com.uade.tpo.demo.entity.CarritoVehiculo;
-
-@Repository
-public interface CarritoVehiculoRepository extends JpaRepository<CarritoVehiculo, Long> {
-}
--- Ecommerce-master/backend/demo/src/main/java/com/uade/tpo/demo/repository/VehicleRepository.java
+++
@@ -1,9 +0,0 @@
-package com.uade.tpo.demo.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import com.uade.tpo.demo.entity.Vehiculo;
-
-@Repository
-public interface VehicleRepository extends JpaRepository<Vehiculo, Long> {
-}
